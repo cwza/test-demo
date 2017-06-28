1af8fd99e8e099b95bcfbb577db4bffff8c89c7c
Ben Wang (Ben) <lbeeon@hotmail.com>
2017-06-28 16:55:47 +0800

    echo api_key

--- .circleci/deploy-heroku.sh
+++ .circleci/deploy-heroku.sh
@@ -1,6 +1,6 @@
 
 # Install docker client
-echo "meepshop"
+echo $HEROKU_API_KEY 
 set -x
 VER="17.03.0-ce"
 curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
